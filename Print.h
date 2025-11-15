77b520db8ecee4b42f94628ec0fce263ee66bdbb
Jules Rotschi <rotschi.jules@gmail.com>
2025-11-15 20:56:03 +0100

    Initial commit

---
+++ Print.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace Prt
+{
+	inline void Print(const std::string& str)
+	{
+		std::cout << str;
+	}
+
+	inline void Print(const std::wstring& wstr)
+	{
+		std::wcout << wstr;
+	}
+
+	inline void Print(char c)
+	{
+		std::cout << c;
+	}
+
+	inline void Print(wchar_t wc)
+	{
+		std::wcout << wc;
+	}
+
+	inline void BreakLine()
+	{
+		Print('\n');
+	}
+
+	inline void BreakLineWide()
+	{
+		Print(L'\n');
+	}
+
+	inline void PrintLine(const std::string& str)
+	{
+		Print(str);
+		BreakLine();
+	}
+
+	inline void PrintLine(const std::wstring& wstr)
+	{
+		Print(wstr);
+		BreakLineWide();
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Print Lib
+
+A tiny C++ library for printing to console.
+
+## Getting started
+
+Copy `Print.h` to your project. Include `Print.h` in the file you want to use the library features.
+
+## Types of string
+
+The library is designed to handle normal strings as well as wide strings (to display characters from UTF-16 charset).
